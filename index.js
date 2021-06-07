cbb43f1241d22b6a852f5fd2a4f1bea562df7a4e
Maki-Grz <mini@ledret.ch>
2021-06-07 14:37:04 +0200

    back-end version001

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/public/index.html`)
-})
-
-io.
+    res.sendFile(`${__dirname}/public/index.html`);
+});
 
 server.listen(8000, () => {
-    console.log('Écoute sur le port: 8000')
-})
+    console.log('Écoute sur le port: 8000');
+});
+
+io.on('connection', (socket) => {
+    console.log('Un user est connecté !');
+
+    socket.on('disconnect', () => {
+        console.log("Un user est déconnecté !")
+    })
+
+    socket.on("tchat_message", (msg) => {
+        io.emit('tchat_message', msg);
+    });
+});
